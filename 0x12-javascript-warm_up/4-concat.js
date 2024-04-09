b6a50e521467089b4d57cf22e7faa5ede562c6ec
rejoiceyobo2 <rejoiceyobo@icloud.com>
2024-04-09 06:45:00 -0700

    4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,3 +1,8 @@
-#!/usr/bin/node
-console.log(process.argv[2] + ' is ' + process.argv[3]);
+#!/usr/bin/env node
 
+// Print two arguments passed to the script in the following format: "is"
+
+const argument1 = process.argv[2];
+const argument2 = process.argv[3];
+
+console.log(`${argument1 || 'is' } ${argument2 || 'undefined'}`);
